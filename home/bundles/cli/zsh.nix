03099a43302afd48debe0e72816171f1c6e62239
Jack Kelly <jpk.lly@gmail.com>
2026-06-05 10:53:46 -0400

    ctrl+left/righ moves by word in alacritty

--- home/bundles/cli/zsh.nix
+++ home/bundles/cli/zsh.nix
@@ -19,8 +19,8 @@
       bindkey  "^[[H"   beginning-of-line
       bindkey  "^[[F"   end-of-line
       bindkey  "^[[3~"  delete-char
-      bindkey "^[[1;3C" forward-word
-      bindkey "^[[1;3D" backward-word
+      bindkey "^[[1;5C" forward-word
+      bindkey "^[[1;5D" backward-word
     '';
   };
 }
