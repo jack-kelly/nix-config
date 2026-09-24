282a1ed1411d1cb5f2b719cb918b7135821aae0b
Jack Kelly <jpk.lly@gmail.com>
2026-09-24 10:10:39 -0400

    bump claude-code pkg to 2.1.281

--- pkgs/claude-code/default.nix
+++ pkgs/claude-code/default.nix
@@ -12,7 +12,7 @@
 }:
 
 let
-  version = "2.1.280";
+  version = "2.1.281";
 
   platformMap = {
     "aarch64-darwin" = "darwin-arm64";
@@ -26,10 +26,10 @@ let
       or (throw "claude-code: unsupported platform ${stdenv.hostPlatform.system}");
 
   nativeHashes = {
-    "darwin-arm64" = "1792an0b1cbm7npwwawjzr78kn7rj6azgfphxn2m10dvrmfmqyiq";
-    "darwin-x64" = "1m3866wmg6skyxznl273180j96s2amwbg2i06c32b0h4cf3jvly1";
-    "linux-x64" = "0nwj4arw2hpyx1rqc47gfqf8sf3ph90g6ckdf06wphpkplym020y";
-    "linux-arm64" = "18j57w0m16pzgk88zkchn9lb7r51yk70wkhdk9xwzgfh0pyv9wlj";
+    "darwin-arm64" = "09pnhgycqxklg657z9jwrg5rjpqscahbm7czxx8s4m9vdwgrh8m9";
+    "darwin-x64" = "124hrpbgx4m0pikmwppschpnf0bl77pscqfgxx4fj7191nxmqdd9";
+    "linux-x64" = "1qfwlvpbaczjjpkjv77va1bxbsizvgfrk4kygnr5yijqhjl3vzjn";
+    "linux-arm64" = "13rjxpk7yr1gbhz48mfsnqlbcwvav8psv6yj1ikx3zm471jb69yx";
   };
 
   nativeBinary = fetchurl {
